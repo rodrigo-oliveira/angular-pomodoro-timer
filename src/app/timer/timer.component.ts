aa8b476d3bc03c3a1d784e167b69564b778db6c5
rodrigo <rodrigo_oliveira@live.com>
2018-12-04 22:04:48 -0200

    Adicionado métodos para o cronômetro

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,20 +1,50 @@
 import {Component} from '@angular/core';
 
+function formatToMinutes(seconds: number) {
+  return (seconds - (seconds %= 60)) / 60 +
+    (9 < seconds ? ':' : ':0') + seconds;
+}
+
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.css']
 })
 export class TimerComponent {
+  paused: Boolean = false;
+  counter: number = 5 * 60;
+  started: Boolean = false;
+  intervalTimer;
+  countdown: String;
 
-  counter = 5 * 60;
+  constructor() {
+    this.countdown = formatToMinutes(this.counter);
+  }
+
+  public timer = () => {
+    if (!this.paused) {
+      this.counter = this.counter - 1;
+      this.countdown = formatToMinutes(this.counter);
+    } else {
+      clearInterval(this.intervalTimer);
+    }
+  }
 
   start() {
-    const timer = () => {
-      this.counter = this.counter - 1;
-      console.log(this.counter);
-    };
-    const intervalTimer = setInterval(timer, 1000);
+    this.paused = false;
+    this.started = true;
+    this.intervalTimer = setInterval(this.timer, 1000);
+  }
 
+  pause() {
+    this.paused = true;
+    this.started = false;
+  }
+
+  reset() {
+    clearInterval(this.intervalTimer);
+    this.started = false;
+    this.counter = 5 * 60;
+    this.countdown = formatToMinutes(this.counter);
   }
 }
